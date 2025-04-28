a7e699484c25a14ad46dee6b58deec8240691603
shahilpateliflair <shahil.patel@iflair.com>
2025-04-28 16:15:43 +0530

    fix issueee

--- src/Controller/Auth/Auth.controller.js
+++ src/Controller/Auth/Auth.controller.js
@@ -5,7 +5,7 @@ const {
 } = require("../../Repository/Auth/Auth.repository");
 
 const signupUser = async (req, res) => {
-  console.log("hhiiiiiiiiii")
+  console.log("hhiiiiiiiiiieeeeeeeeeeeee")
   try {
     const reqData = req.body;
     const SignUpResponse = await signup(reqData);

31d9bfc70f038c305169c649df105d60928dd8b3
deepraj_chouhan <161432600+DeepRajBytes@users.noreply.github.com>
2025-04-29 13:52:14 +0530

    Update mysql.js

--- src/Connections/Database/mysql.js
+++ src/Connections/Database/mysql.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
 const util = require("util");
+const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../.env") });
 console.log(
   "env",

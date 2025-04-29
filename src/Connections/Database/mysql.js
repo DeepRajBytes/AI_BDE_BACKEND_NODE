1af8c8c353515e90aa1974b256eb69758b0cea8b
deepraj_chouhan <deepraj.chouhan@iflair.com>
2025-04-29 13:58:15 +0530

    make some changes

--- src/Connections/Database/mysql.js
+++ src/Connections/Database/mysql.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 const util = require("util");
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, "../.env") });
+
 console.log(
   "env",
   process.env.LOCAL_HOST,
